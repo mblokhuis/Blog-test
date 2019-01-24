2a427c58557606a80db1440fe19c1512b33d117b
Martijn Blokhuis <blokhuis.m@gmail.com>
2019-01-24 16:21:52 +0100

    First commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    
+    <title>Page Title</title>
+    
+</head>
+
+<body>
+
+    <h1>Contact Form </h1>    
+
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    
+    <title>Page Title</title>
+    
+</head>
+
+<body>
+
+    <h1>Here we go!!!!! </h1>    
+
+<ul>
+
+<li><a href="/about">About us</a> us to learn more.</li>
+<li><a href="/contact">Contact</a> us to learn more.</li>
+
+
+<ul/>
+
+</body>
+
+</html>
