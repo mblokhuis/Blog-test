e54262409d4223d71fe8a7a997d36b95b13a3c46
Martijn Blokhuis <blokhuis.m@gmail.com>
2019-01-24 16:28:04 +0100

    test
    test file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,8 +14,10 @@
 
 <ul>
 
-<li><a href="/about">About us</a> us to learn more.</li>
+<li><a href="/about">About us</a></li>
 <li><a href="/contact">Contact</a> us to learn more.</li>
+
+test
 
 
 <ul/>
